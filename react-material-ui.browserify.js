0c6098821f09b14cbabf6e16e71eb234d62f4a99
flake <raj.cynosure@gmail.com>
2015-11-25 10:56:19 +0530

    Update react-material-ui.browserify.js

--- react-material-ui.browserify.js
+++ react-material-ui.browserify.js
@@ -3,3 +3,10 @@ global.React = React;
 
 injectTapEventPlugin = require('react-tap-event-plugin');
 mui = require('material-ui');
+
+mui.Libs = {};
+MUI.Libs.Menu = require('material-ui/lib/menus/menu');
+MUI.Libs.MenuItem = require('material-ui/lib/menus/menu-item');
+MUI.Libs.MenuDivider = require('material-ui/lib/menus/menu-divider');
+
+MUI.Libs.SvgIcons = require('material-ui/lib/svg-icons');
